946507acb7a54f9a9be9aa0f30249c8ad81f7070
Fire2heart <52fire2heart52@gmail.com>
2022-02-12 18:11:31 +0200

    First commit

---
+++ src/scripts/main.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const li = document.querySelectorAll('li');
+const mainImage = document.querySelector('div').firstElementChild.firstElementChild;
+
+const showImg = (event) => {
+    const imageToChange = event.target.parentNode;
+    mainImage.src = imageToChange.href;
+}
+
+const div = document.querySelector('#largeImg');
+console.log(div.srс);
+
+li.forEach(element => {
+    element.addEventListener('click', showImg);
+})
+
+const img = document.querySelectorAll('img');
+
+const cancel = (event) => {
+    event.preventDefault();
+}
+
+img.forEach(element => {
+    element.addEventListener('click', cancel);
+})
